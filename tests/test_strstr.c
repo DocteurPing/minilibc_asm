bdef514c34177fbc283c2c0831762c9e8233f02a
drping <cedric.ogire@epitech.eu>
2018-03-19 15:18:30 +0100

    ASM: add test for strstr

---
+++ tests/test_strstr.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2018
+** ASM_minilibc_2017
+** File description:
+** test_strstr
+*/
+
+#include <string.h>
+#include <criterion/criterion.h>
+
+Test(my_strstr, easy)
+{
+	char *str;
+
+	str = strstr("salut tout le monde", "tout");
+	cr_assert(str, "tout le monde");
+}
+
+Test(my_strstr, no_match)
+{
+	char *str;
+
+	str = strstr("salut tout le monde", "z");
+	cr_assert_null(str);
+}
