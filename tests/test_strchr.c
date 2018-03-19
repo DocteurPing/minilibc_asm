d7ce1dc0864939808f8986b7ffae765ea34bed23
drping <cedric.ogire@epitech.eu>
2018-03-19 12:20:14 +0100

    ASM: add test for strchr

---
+++ tests/test_strchr.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2018
+** ASM_minilibc_2017
+** File description:
+** test_strchr
+*/
+
+#include <criterion/criterion.h>
+#include "stdio.h"
+
+Test(my_strchr, easy)
+{
+	char    motA[100] = "Bravo";
+	char    *pointeurLettreA = strchr(motA, 'a');
+
+	*pointeurLettreA = 'e';
+	cr_assert(motA, "Brevo");
+}
+
+Test(my_strchr, nullbyte)
+{
+	char    motA[100] = "Bravo";
+	char    *pointeurLettre0 = strchr(motA, 0);
+	
+	*pointeurLettre0 = 'c';
+	cr_assert(motA, "Bravoc");
+}
